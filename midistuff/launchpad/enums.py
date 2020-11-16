e55388defaa995923afde7d1384b09ace30cc87f
Katistix <33645731+Katistic@users.noreply.github.com>
2020-11-16 15:24:14 +1100

    Get layout name

--- midistuff/launchpad/enums.py
+++ midistuff/launchpad/enums.py
@@ -6,6 +6,14 @@ class MK2Layout:
     Volume = 4
     Pan = 5
 
+    @staticmethod
+    def layout_name(self, t):
+        layouts = ["Session", "User 1", "User 2", "Ableton", "Volume", "Pan"]
+        if t < len(layouts) and t >= 0:
+            return layouts[t]
+
+        raise Errors.InvalidLayout("Layout does not exist!")
+
 class Errors:
     class WrongLayout(Exception):
         pass
