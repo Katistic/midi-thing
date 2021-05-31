e80c1d408e3cff824f9968ed69d5f75e11e4aad7
Kat <katistix@gmail.com>
2021-05-31 17:21:02 +1000

    KeySignatureKey enum

--- midistuff/midi_parser/enums.py
+++ midistuff/midi_parser/enums.py
@@ -4,3 +4,21 @@ import enum
 class KeySignatureScale(enum.Enum):
     MAJOR = 0
     MINOR = 1
+
+
+class KeySignatureKey(enum.Enum):
+    CF = -7
+    GF = -6
+    DF = -5
+    AF = -4
+    EF = -3
+    BF = -2
+    F = -1
+    C = 0
+    G = 1
+    D = 2
+    A = 3
+    E = 4
+    B = 5
+    FS = 6
+    CS = 7
